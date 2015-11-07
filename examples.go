493f54a085420bebda56aa6e3a531df55fbad4fa
NaySoftware <nay.software@gmail.com>
2015-11-07 16:18:21 +0200

    examples

---
+++ examples.go
@@ -0,0 +1,55 @@
+package gcm
+
+
+import (
+  "gcm"
+  "fmt"
+)
+
+const (
+  GCM_API_KEY = "GCM_API_KEY"
+
+)
+
+
+func main() {
+
+  p := &gcm.NotificationPayload{
+    Title : "Android",
+    Body: "Hello Android",
+    Icon: "ic_stat_ic_notification",
+    ClickAction: "OPEN_ACTIVITY_1",
+    Sound: "default",
+    Tag: "ab",
+  }
+
+
+  ids := []string{
+    "GCM_TOKEN1",
+    "GCM_TOKEN2",
+  }
+
+  // data := map[string]string{
+  //   "message": "Hello Xman",
+  // }
+
+  client := gcm.NewGcmClient(GCM_API_KEY)
+
+  // data message exmaple
+  // status, err := client.NewDevicesList(ids).SetMsgData(data).Send()
+
+  status, err := client.NewDevicesList(ids).SetNotificationPayload(p).Send()
+
+  if err == nil {
+
+    // fmt.Println(status.MulticastId)
+    fmt.Println("Success:", status.Success)
+    fmt.Println("Fail:", status.Fail)
+    fmt.Println("Canonical_ids:",status.Canonical_ids)
+    // fmt.Println(status.Results)
+  } else {
+    fmt.Println(err)
+  }
+
+
+}
