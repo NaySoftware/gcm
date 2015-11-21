5b199bbd6e2fdbbe044b88f0e3f7cccdaaecf4a1
NaySoftware <nay.software@gmail.com>
2015-11-21 17:13:29 +0200

    go fmt

--- gcm.go
+++ gcm.go
@@ -1,264 +1,244 @@
-
 package gcm
 
 import (
-  "fmt"
-  "net/http"
-  "io/ioutil"
-  "bytes"
-  // "errors"
-  "encoding/json"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	// "errors"
+	"encoding/json"
 )
 
 const (
-  server_url = "https://gcm-http.googleapis.com/gcm/send"
-  MAX_TTL = 2419200
-  Priority_HIGH   = "high"
-  Priority_NORMAL = "normal"
+	server_url      = "https://gcm-http.googleapis.com/gcm/send"
+	MAX_TTL         = 2419200
+	Priority_HIGH   = "high"
+	Priority_NORMAL = "normal"
 )
 
 type GcmClient struct {
-  ApiKey string
-  Message GcmMsg
+	ApiKey  string
+	Message GcmMsg
 }
 
 type GcmMsg struct {
-  Data map[string]string            `json:"data,omitempty"`
-  To string                         `json:"to,omitempty"`
-  RegistrationIds []string          `json:"registration_ids,omitempty"`
-  CollapseKey   string              `json:"collapse_key,omitempty"`
-  Priority string                   `json:"priority,omitempty"`
-  Notification NotificationPayload  `json:"notification,omitempty"`
-  ContentAvailable bool             `json:"content_available,omitempty"`
-  DelayWhileIdle  bool              `json:"delay_while_idle,omitempty"`
-  TimeToLive    int                 `json:"time_to_live,omitempty"`
-  RestrictedPackageName string      `json:"restricted_package_name,omitempty"`
-  DryRun      bool                  `json:"dry_run,omitempty"`
-
+	Data                  map[string]string   `json:"data,omitempty"`
+	To                    string              `json:"to,omitempty"`
+	RegistrationIds       []string            `json:"registration_ids,omitempty"`
+	CollapseKey           string              `json:"collapse_key,omitempty"`
+	Priority              string              `json:"priority,omitempty"`
+	Notification          NotificationPayload `json:"notification,omitempty"`
+	ContentAvailable      bool                `json:"content_available,omitempty"`
+	DelayWhileIdle        bool                `json:"delay_while_idle,omitempty"`
+	TimeToLive            int                 `json:"time_to_live,omitempty"`
+	RestrictedPackageName string              `json:"restricted_package_name,omitempty"`
+	DryRun                bool                `json:"dry_run,omitempty"`
 }
 
 type GcmResponseStatus struct {
-  Ok bool   
-  StatusCode    int
-  MulticastId int                   `json:"multicast_id"`
-  Success int                       `json:"success"`
-  Fail int                          `json:"failure"`
-  Canonical_ids int                 `json:"canonical_ids"`
-  Results []map[string]string       `json:"results,omitempty"`
-} 
-
+	Ok            bool
+	StatusCode    int
+	MulticastId   int                 `json:"multicast_id"`
+	Success       int                 `json:"success"`
+	Fail          int                 `json:"failure"`
+	Canonical_ids int                 `json:"canonical_ids"`
+	Results       []map[string]string `json:"results,omitempty"`
+}
 
 type NotificationPayload struct {
-  Title string   `json:"title,omitempty"`
-  Body  string   `json:"body,omitempty"`
-  Icon  string   `json:"icon,omitempty"`
-  Sound string   `json:"sound,omitempty"`
-  Badge string   `json:"badge,omitempty"`
-  Tag   string   `json:"tag,omitempty"`
-  Color string   `json:"color,omitempty"`
-  ClickAction string  `json:"click_action,omitempty"`
-  BodyLocKey  string  `json:"body_loc_key,omitempty"`
-  BodyLocArgs string  `json:"body_loc_args,omitempty"`
-  TitleLocKey string  `json:"title_loc_key,omitempty"`
-  TitleLocArgs string `json:"title_loc_args,omitempty"`
+	Title        string `json:"title,omitempty"`
+	Body         string `json:"body,omitempty"`
+	Icon         string `json:"icon,omitempty"`
+	Sound        string `json:"sound,omitempty"`
+	Badge        string `json:"badge,omitempty"`
+	Tag          string `json:"tag,omitempty"`
+	Color        string `json:"color,omitempty"`
+	ClickAction  string `json:"click_action,omitempty"`
+	BodyLocKey   string `json:"body_loc_key,omitempty"`
+	BodyLocArgs  string `json:"body_loc_args,omitempty"`
+	TitleLocKey  string `json:"title_loc_key,omitempty"`
+	TitleLocArgs string `json:"title_loc_args,omitempty"`
+}
+
+func NewGcmClient(apiKey string) *GcmClient {
+	gcmc := new(GcmClient)
+	gcmc.ApiKey = apiKey
+
+	return gcmc
+}
+
+func (this *GcmClient) NewGcmMsgTo(to string, body map[string]string) *GcmClient {
+	this.Message.To = to
+	this.Message.Data = body
+
+	return this
+}
+
+func (this *GcmClient) SetMsgData(body map[string]string) *GcmClient {
+
+	this.Message.Data = body
+
+	return this
 
 }
 
-func NewGcmClient(apiKey string) (*GcmClient) {
-  gcmc := new(GcmClient)
-  gcmc.ApiKey = apiKey
+func (this *GcmClient) NewGcmRegIdsMsg(list []string, body map[string]string) *GcmClient {
+	this.NewDevicesList(list)
+	this.Message.Data = body
 
-  return gcmc
-}
-
-
-func (this *GcmClient) NewGcmMsgTo(to string, body map[string]string) (*GcmClient) {
-  this.Message.To = to
-  this.Message.Data = body
-
-  return this
-}
-
-func (this *GcmClient) SetMsgData(body map[string]string) (*GcmClient) {
-  
-  this.Message.Data = body
-
-  return this
+	return this
 
 }
 
+func (this *GcmClient) NewDevicesList(list []string) *GcmClient {
+	this.Message.RegistrationIds = make([]string, len(list))
+	copy(this.Message.RegistrationIds, list)
 
-
-func (this *GcmClient) NewGcmRegIdsMsg(list []string, body map[string]string) (*GcmClient) {
-  this.NewDevicesList(list)
-  this.Message.Data = body
-
-  return this
+	return this
 
 }
 
+func (this *GcmClient) AppendDevices(list []string) *GcmClient {
 
-func (this *GcmClient) NewDevicesList(list []string) (*GcmClient) {
-  this.Message.RegistrationIds = make([]string, len(list))
-  copy(this.Message.RegistrationIds, list)
+	this.Message.RegistrationIds = append(this.Message.RegistrationIds, list...)
 
-  return this
-
+	return this
 }
-
-func (this *GcmClient) AppendDevices(list []string) (*GcmClient)  {
-
-  this.Message.RegistrationIds = append(this.Message.RegistrationIds, list...)
-
-  return this
-}
-
 
 func (this *GcmClient) Send() (*GcmResponseStatus, error) {
 
-  gcmRespStatus := new(GcmResponseStatus)
+	gcmRespStatus := new(GcmResponseStatus)
 
+	jsonByte, err := this.Message.toJsonByte()
+	if err != nil {
+		gcmRespStatus.Ok = false
+		return gcmRespStatus, err
+	}
 
-  jsonByte , err := this.Message.toJsonByte()
-  if err != nil {
-    gcmRespStatus.Ok = false
-    return gcmRespStatus, err 
-  }
+	// fmt.Println(string(jsonByte))
 
-  // fmt.Println(string(jsonByte))
+	request, err := http.NewRequest("POST", server_url, bytes.NewBuffer(jsonByte))
+	request.Header.Set("Authorization", this.apiKeyHeader())
+	request.Header.Set("Content-Type", "application/json")
 
-  request, err := http.NewRequest("POST", server_url , bytes.NewBuffer(jsonByte))
-  request.Header.Set("Authorization", this.apiKeyHeader())
-  request.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	response, err := client.Do(request)
 
+	if err != nil {
+		gcmRespStatus.Ok = false
+		return gcmRespStatus, err
+	}
+	defer response.Body.Close()
 
-  client := &http.Client{}
-  response, err := client.Do(request)
+	body, err := ioutil.ReadAll(response.Body)
 
-  if err != nil {
-    gcmRespStatus.Ok = false
-    return gcmRespStatus, err
-  }
-  defer response.Body.Close()
+	gcmRespStatus.StatusCode = response.StatusCode
 
+	if response.StatusCode == 200 && err == nil {
 
-  body, err := ioutil.ReadAll(response.Body)
+		gcmRespStatus.Ok = true
 
-  gcmRespStatus.StatusCode = response.StatusCode
+		// fmt.Println(response.Status)
+		eror := gcmRespStatus.parseStatusBody(body)
+		if eror != nil {
+			return gcmRespStatus, eror
+		}
 
-  if response.StatusCode == 200 && err == nil {
+		return gcmRespStatus, nil
 
+	} else {
+		gcmRespStatus.Ok = false
 
-  gcmRespStatus.Ok = true
+		eror := gcmRespStatus.parseStatusBody(body)
+		if eror != nil {
+			return gcmRespStatus, eror
+		}
 
-  // fmt.Println(response.Status)
-  eror := gcmRespStatus.parseStatusBody(body)
-  if eror != nil {
-    return gcmRespStatus, eror
-  }
+		return gcmRespStatus, err
 
-  return gcmRespStatus, nil
-
-  } else  {
-    gcmRespStatus.Ok = false
-
-    eror := gcmRespStatus.parseStatusBody(body)
-  if eror != nil {
-    return gcmRespStatus, eror
-  }
-
-    return gcmRespStatus, err 
-
-  }
+	}
 
 }
 
 func (this *GcmMsg) toJsonByte() ([]byte, error) {
 
-  return json.Marshal(this)
+	return json.Marshal(this)
 
 }
 
+func (this *GcmResponseStatus) parseStatusBody(body []byte) error {
 
-func (this *GcmResponseStatus) parseStatusBody(body []byte ) (error)  {
-
-  if err := json.Unmarshal([]byte(body), &this); err != nil {
-    return err
-  } 
-  return nil
+	if err := json.Unmarshal([]byte(body), &this); err != nil {
+		return err
+	}
+	return nil
 
 }
 
 func (this *GcmClient) apiKeyHeader() string {
-  return fmt.Sprintf("key=%v", this.ApiKey)
+	return fmt.Sprintf("key=%v", this.ApiKey)
 }
 
 func (this *GcmClient) SetPriorety(p string) {
 
-  if p == Priority_HIGH {
-  this.Message.Priority = Priority_HIGH
-  } else {
-    this.Message.Priority = Priority_NORMAL
-  }
+	if p == Priority_HIGH {
+		this.Message.Priority = Priority_HIGH
+	} else {
+		this.Message.Priority = Priority_NORMAL
+	}
 }
 
-func (this *GcmClient) SetCollapseKey(val string) (*GcmClient) {
+func (this *GcmClient) SetCollapseKey(val string) *GcmClient {
 
-  this.Message.CollapseKey = val
+	this.Message.CollapseKey = val
 
-  return this
+	return this
 }
 
-func (this *GcmClient) SetNotificationPayload(payload *NotificationPayload) (*GcmClient) {
+func (this *GcmClient) SetNotificationPayload(payload *NotificationPayload) *GcmClient {
 
-  this.Message.Notification = *payload
+	this.Message.Notification = *payload
 
-  return this
+	return this
 }
 
-func (this *GcmClient) SetContentAvailable(isContentAvailable bool) (*GcmClient) {
-  
-  this.Message.ContentAvailable = isContentAvailable
+func (this *GcmClient) SetContentAvailable(isContentAvailable bool) *GcmClient {
 
-  return this
+	this.Message.ContentAvailable = isContentAvailable
+
+	return this
 }
 
-func (this *GcmClient) SetDelayWhileIdle(isDelayWhileIdle bool) (*GcmClient) {
-  
-  this.Message.DelayWhileIdle = isDelayWhileIdle
+func (this *GcmClient) SetDelayWhileIdle(isDelayWhileIdle bool) *GcmClient {
 
-  return this
+	this.Message.DelayWhileIdle = isDelayWhileIdle
+
+	return this
 }
-func (this *GcmClient) SetTimeToLive(ttl int) (*GcmClient) {
-  
-  if ttl > MAX_TTL {
-    
-    this.Message.TimeToLive = MAX_TTL
+func (this *GcmClient) SetTimeToLive(ttl int) *GcmClient {
 
-   } else {
+	if ttl > MAX_TTL {
 
-    this.Message.TimeToLive = ttl
+		this.Message.TimeToLive = MAX_TTL
 
-   }
-  return this
-}
+	} else {
 
-func (this *GcmClient) SetRestrictedPackageName(pkg string) (*GcmClient) {
-  
-  this.Message.RestrictedPackageName = pkg
+		this.Message.TimeToLive = ttl
 
-  return this
+	}
+	return this
 }
 
+func (this *GcmClient) SetRestrictedPackageName(pkg string) *GcmClient {
 
-func (this *GcmClient) SetDryRun(drun bool ) (*GcmClient) {
-  
-  this.Message.DryRun = drun
+	this.Message.RestrictedPackageName = pkg
 
-  return this
+	return this
 }
 
+func (this *GcmClient) SetDryRun(drun bool) *GcmClient {
 
+	this.Message.DryRun = drun
 
-
-
+	return this
+}
